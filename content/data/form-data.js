76803ec5f73358b5e1be9334136c4778a4609227
Joshua Simpson <jsimpson@financeit.io>
2018-07-10 10:34:27 -0400

    Add checkboxes and radio buttons

---
+++ content/data/form-data.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const formData = {
+  verticals: [
+    {
+      title: 'Home Improvement',
+      label: 'HVAC, solar, windows & doors. pool & spa, roofing, and contracting',
+      name: 'homeImprovementCheckbox'
+    },
+    {
+      title: 'Retail',
+      label: 'Furniture, appliances, electronics, sports equipment, consumer goods, and automotive parts & repairs',
+      name: 'retailCheckbox'
+    },
+    {
+      title: 'Powersports',
+      label: 'ATV, scooter, snowmobile, and motorcycle',
+      name: 'powersportsCheckbox'
+    },
+    {
+      title: 'Marine, RV & Trailers',
+      label: 'Boat, trailer, and RV',
+      name: 'marineRVTrailersCheckbox'
+    },
+    {
+      title: 'Auto',
+      label: 'Car, truck and SUV',
+      name: 'autoCheckbox'
+    }
+  ]
+};
+
+module.exports = formData;
